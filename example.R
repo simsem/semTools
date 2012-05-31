a5f77dc50b6a280efe10140a685d9e9346215595
Sunthud Pornprasertmanit <psunthud@gmail.com>
2012-05-30 23:07:19 -0500

    Add miPowerFit function

--- example.R
+++ example.R
@@ -72,3 +72,34 @@ dat<-data.frame(cbind(x1,x2,x3,z1,z2,z3,y1,y2,y3))
 datOrth <-orthogonalize(dat,(1:3), (4:6))
 
 ########### Better have the runMI example
+
+########### Saris, Satorra, and van der Veld (2009)
+
+library(lavaan)
+
+HS.model <- ' visual  =~ x1 + x2 + x3
+              textual =~ x4 + x5 + x6
+              speed   =~ x7 + x8 + x9 '
+
+fit <- cfa(HS.model, data=HolzingerSwineford1939, group="sex", meanstructure=TRUE)
+miPowerFit(fit)
+
+model <- ' 
+  # latent variable definitions
+     ind60 =~ x1 + x2 + x3
+     dem60 =~ y1 + a*y2 + b*y3 + c*y4
+     dem65 =~ y5 + a*y6 + b*y7 + c*y8
+
+  # regressions
+    dem60 ~ ind60
+    dem65 ~ ind60 + dem60
+
+  # residual correlations
+    y1 ~~ y5
+    y2 ~~ y4 + y6
+    y3 ~~ y7
+    y4 ~~ y8
+    y6 ~~ y8
+'
+fit2 <- sem(model, data=PoliticalDemocracy, meanstructure=TRUE)
+miPowerFit(fit2, stdLoad=0.3, cor=0.2, stdBeta=0.2, intcept=0.5)
---
+++ semTools/R/miPowerFit.R
@@ -0,0 +1,162 @@
+# miPowerFit: Evaluate model fit by Satorra, Saris, & van der Weld (2009) method
+
+miPowerFit <- function(lavaanObj, delta=NULL, stdDelta=NULL, stdLoad=0.4, cor=0.1, stdBeta=0.1, intcept=0.2) {
+	library(lavaan)
+	mi <- inspect(lavaanObj, "mi")
+	sigma <- mi[,"epc"] / sqrt(mi[,"mi"])
+	if(is.null(delta)) {
+		if(is.null(stdDelta)) stdDelta <- getTrivialEpc(mi, stdLoad=stdLoad, cor=cor, stdBeta=stdBeta, intcept=intcept)
+		if(length(stdDelta) == 1) stdDelta <- rep(stdDelta, nrow(mi)) 
+		delta <- unstandardizeEpc(mi, stdDelta, findTotalVar(lavaanObj))
+	}
+	if(length(delta) == 1) delta <- rep(delta, nrow(mi))
+	ncp <- (delta / sigma)^2
+	alpha <- 0.05
+	desiredPow <- 0.80
+	cutoff <- qchisq(1 - alpha, df = 1)
+	pow <- 1 - pchisq(cutoff, df = 1, ncp=ncp)
+	sigMI <- mi[,"mi"] > cutoff
+	highPow <- pow > desiredPow
+	group <- rep(1, nrow(mi))
+	if("group" %in% colnames(mi)) group <- mi[,"group"]
+	decision <- mapply(decisionMIPow, sigMI=sigMI, highPow=highPow, epc=mi[,"epc"], trivialEpc=delta)
+	if(is.null(stdDelta)) stdDelta <- standardizeEpc(mi, findTotalVar(lavaanObj), delta=delta)
+	result <- cbind(mi[,1:3], group, as.numeric(mi[,"mi"]), mi[,"epc"], delta, standardizeEpc(mi, findTotalVar(lavaanObj)), stdDelta, sigMI, highPow, decision)
+	
+	result <- result[!is.na(decision),]
+	colnames(result) <- c("lhs", "op", "rhs", "group", "mi", "epc", "target.epc", "std.epc", "std.target.epc", "significant.mi", "high.power", "decision")
+	result <- format(result, scientific=FALSE, digits=4)
+	return(result)
+}
+
+# totalFacVar: Find total factor variances when regression coeffient matrix and factor residual covariance matrix are specified
+
+totalFacVar <- function(beta, psi) {
+	ID <- diag(nrow(psi))
+    total <- solve(ID - beta) %*% psi %*% t(solve(ID - beta))
+    return(diag(total))
+}
+
+# findTotalVar: find the total indicator and factor variances
+
+findTotalVar <- function(lavaanObj) {
+	result <- list()
+	nGroups <- lavaanObj@Model@ngroups
+	posPsi <- which(names(lavaanObj@Model@GLIST) == "psi")
+	posBeta <- which(names(lavaanObj@Model@GLIST) == "beta")
+	isCfa <- length(posBeta) == 0
+	dimNames <- unlist(lavaanObj@Model@dimNames[[1]])
+	for(i in 1:nGroups) {
+		temp <- diag(lavaanObj@Fit@Sigma.hat[[i]])
+		if(isCfa) {
+			temp <- c(temp, diag(lavaanObj@Model@GLIST[[posPsi[i]]]))
+		} else {
+			groupPsi <- lavaanObj@Model@GLIST[[posPsi[i]]]
+			groupBeta <- lavaanObj@Model@GLIST[[posBeta[i]]]
+			facCov <- totalFacVar(groupBeta, groupPsi)
+			temp <- c(temp, facCov)
+		}
+		names(temp) <- dimNames
+		result[[i]] <- temp
+	}
+	return(result)
+}
+
+# getTrivialEpc: find the trivial misspecified expected parameter changes given the type of parameters in each row of modification indices
+
+getTrivialEpc <- function(mi, stdLoad=0.4, cor=0.1, stdBeta=0.1, intcept=0.2) {
+	op <- mi[,"op"]
+	result <- gsub("=~", stdLoad, op)
+	result <- gsub("~~", cor, result)
+	result <- gsub("~1", intcept, result)
+	result <- gsub("~", stdBeta, result)
+	return(result)
+}
+
+# unstandardizeEpc: Transform from standardized EPC to unstandardized EPC
+
+unstandardizeEpc <- function(mi, delta, totalVar) {
+	name <- names(totalVar[[1]])
+	lhsPos <- match(mi[,"lhs"], name)
+	rhsPos <- match(mi[,"rhs"], name)
+	group <- rep(1, nrow(mi))
+	if("group" %in% colnames(mi)) group <- mi[,"group"]
+	getVar <- function(pos, group) totalVar[[group]][pos]
+	lhsVar <- mapply(getVar, pos=lhsPos, group=group)
+	rhsVar <- mapply(getVar, pos=rhsPos, group=group)
+	FUN <- function(op, lhsVar, rhsVar, delta) {
+		lhsSD <- sqrt(lhsVar)
+		rhsSD <- sqrt(rhsVar)
+		if(!is.numeric(delta)) delta <- as.numeric(delta)
+		if(op == "=~") {
+			return((rhsSD * delta) / lhsSD)
+		} else if (op == "~~") {
+			return(lhsSD * delta * rhsSD)
+		} else if (op == "~1") {
+			return(lhsSD * delta)
+		} else if (op == "~") {
+			return((lhsSD * delta) / rhsSD)
+		} else {
+			return(NA)
+		}
+	}
+	unstdDelta <- mapply(FUN, op=mi[,"op"], lhsVar=lhsVar, rhsVar=rhsVar, delta=delta)
+	return(unstdDelta)
+}
+
+# unstandardizeEpc: Transform from unstandardized EPC to standardized EPC. If delta is null, the unstandardized epc from the modification indices data.frame are used
+
+standardizeEpc <- function(mi, totalVar, delta=NULL) {
+	if(is.null(delta)) delta <- mi[,"epc"]
+	name <- names(totalVar[[1]])
+	lhsPos <- match(mi[,"lhs"], name)
+	rhsPos <- match(mi[,"rhs"], name)
+	group <- rep(1, nrow(mi))
+	if("group" %in% colnames(mi)) group <- mi[,"group"]
+	getVar <- function(pos, group) totalVar[[group]][pos]
+	lhsVar <- mapply(getVar, pos=lhsPos, group=group)
+	rhsVar <- mapply(getVar, pos=rhsPos, group=group)
+	FUN <- function(op, lhsVar, rhsVar, delta) {
+		lhsSD <- sqrt(lhsVar)
+		rhsSD <- sqrt(rhsVar)
+		if(!is.numeric(delta)) delta <- as.numeric(delta)
+		if(op == "=~") {
+			#stdload = beta * sdlatent / sdindicator = beta * lhs / rhs
+			return((delta / rhsSD) * lhsSD)
+		} else if (op == "~~") {
+			#r = cov / (sd1 * sd2)
+			return(delta / (lhsSD * rhsSD))
+		} else if (op == "~1") {
+			#d = meanDiff/sd
+			return(delta / lhsSD)
+		} else if (op == "~") {
+			#beta = b * sdX / sdY = b * rhs / lhs
+			return((delta / lhsSD) * rhsSD)
+		} else {
+			return(NA)
+		}
+	}
+	stdDelta <- mapply(FUN, op=mi[,"op"], lhsVar=lhsVar, rhsVar=rhsVar, delta=delta)
+	return(stdDelta)
+}
+
+# decisionMIPow: provide the decision given the significance of modification indices and power to detect trivial misspecification
+
+decisionMIPow <- function(sigMI, highPow, epc, trivialEpc) {
+	if(is.na(sigMI) | is.na(highPow)) return(NA)
+	if(sigMI & highPow) {
+		if(abs(epc) > abs(trivialEpc)) {
+			return("EPC:M")
+		} else {
+			return("EPC:NM")
+		}
+	} else if (sigMI & !highPow) {
+		return("M")
+	} else if (!sigMI & highPow) {
+		return("NM")
+	} else if (!sigMI & !highPow) {
+		return("I")
+	} else {
+		return(NA)
+	}
+}
