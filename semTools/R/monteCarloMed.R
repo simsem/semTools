ebad98592a607b7ff54bfd60d0f0fd29eedd9767
Alexander Schoemann <schoemann@ku.edu>
2012-06-05 11:35:18 -0500

    add ACOV arguemnt to monteCarloMed

--- semTools/R/monteCarloMed.R
+++ semTools/R/monteCarloMed.R
@@ -3,14 +3,11 @@
 ## Function that takes an expression for an indirect effect, related parameter estimates and SEs and outputs a Monte Carlo SE
 ##Output: matrix of LL and UL, optional plot of indirect effect, or values of indirect effect.
 
-monteCarloMed<-function(expression, ..., rep=20000, CI=95, plot=FALSE, outputValues=FALSE){
+monteCarloMed<-function(expression, ..., rep=20000, CI=95, plot=FALSE, outputValues=FALSE, ACOV=NULL){
   input<- c(...)
-  
-  #Get coefficents and SE from user input
-  means<-as.list(input[seq(1,length(input),2)])
-  sds<-as.list(input[seq(2,length(input),2)])
-  
-  #Get names and the number of unique variables in the expression
+ # input<-c(1,.5,2,.7)
+ 
+ #Get names and the number of unique variables in the expression
   uniquepar<-function(var){
     var<-gsub(" ","",var) 
     var<-strsplit(var,'+',fixed=TRUE)
@@ -22,13 +19,38 @@ monteCarloMed<-function(expression, ..., rep=20000, CI=95, plot=FALSE, outputVal
         var<-unique(varb)}
     if(is.list(var)){var<-var[[1]]}
     return(var)}
-  names(means)<-uniquepar(expression)
+	
+  #Get coefficents and SE from user input
+  if(is.null(ACOV)){
+  means<-as.list(input[seq(1,length(input),2)])
+  sds<-as.list(input[seq(2,length(input),2)])
+   names(means)<-uniquepar(expression)
   names(sds)<-paste(names(means),"sd",sep="")
-  vecs<-list()
+  }
+  if(is.matrix(ACOV)){
+  means<-as.list(input)
+  names(means)<-uniquepar(expression)
+  }
   
+  
+  #names(means)<-uniquepar(expression)
+  #names(sds)<-paste(names(means),"sd",sep="")
+  vecs<-list()
+    #Use ACOV matrix if provided
+    if(is.matrix(ACOV)){
+	require(MASS)
+	#Matrix of values, need to be converted to a list
+	dat <- mvrnorm(n=rep, mu=unlist(means), Sigma=ACOV)
+	#Convert to a list,
+	vecs<-as.list(as.data.frame(dat))
+	}
+	if(is.null(ACOV)){
   #Simulate values of all parameters for all reps
   for(i in 1:length(means)){vecs[[i]]<-rnorm(rep)*sds[[i]]+means[[i]]}
+  }
+  #Give names to it works with assign
   for(i in 1:length(vecs)){assign(names(means)[i],vecs[[i]])}
+  
   #Apply the expression to compute the indirect effect
   indirect<-eval(parse(text=expression))
   #Get the CI
@@ -41,7 +63,7 @@ monteCarloMed<-function(expression, ..., rep=20000, CI=95, plot=FALSE, outputVal
   
   #Switch for outputting a plot
   if(plot) {
-  hist(indirect,breaks='FD',col='skyblue',xlab=paste(conf,'% Confidence Interval ','LL',LL,'  UL',UL), main='Distribution of Indirect Effect')
+  hist(indirect,breaks='FD',col='skyblue',xlab=paste(CI,'% Confidence Interval ','LL',LL,'  UL',UL), main='Distribution of Indirect Effect')
   }
   
   #Switch to return simulated values
@@ -53,13 +75,19 @@ monteCarloMed<-function(expression, ..., rep=20000, CI=95, plot=FALSE, outputVal
 }
 
 #test simple mediation
-# med <- 'a*b'
-# ac <- 1
-# ase <- .01
-# bc<-2
-# bse <- .02
+ med <- 'a*b'
+ ac <- 1
+ ase <- .01
+ bc<-2
+ bse <- .02
 
-# monteCarloMed(med, coef1=ac, se1=ase, coef2=bc, se2=bse, outputValues=F, plot=T)
+
+ monteCarloMed(med, coef1=ac, se1=ase, coef2=bc, se2=bse, outputValues=F, plot=T)
+ 
+AC <- matrix(c(.01,.00002,.00002,.02), nrow=2)
+#If assymptotic covariance matrix is provided, SEs do not need to be provided seperately.
+monteCarloMed(med, coef1=ac, coef2=bc, outputValues=F, plot=F, ACOV=AC)
+
 
 #test complex mediation
 # med <- 'a1*b2 + a1*b1'
