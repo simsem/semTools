8a4c7ae4ad97487835bc842e7bcf24eeca0741d2
Sunthud Pornprasertmanit <psunthud@gmail.com>
2012-06-05 15:32:43 -0500

    0.1-4

--- semTools.Rcheck/semTools-Ex.R
+++ semTools.Rcheck/semTools-Ex.R
@@ -86,6 +86,53 @@ miPowerFit(fit2, stdLoad=0.3, cor=0.2, stdBeta=0.2, intcept=0.5)
 
 
 cleanEx()
+nameEx("monteCarloMed")
+### * monteCarloMed
+
+flush(stderr()); flush(stdout())
+
+### Name: monteCarloMed
+### Title: Monte Carlo Confidence Intervals to Test Complex Indirect
+###   Effects
+### Aliases: monteCarloMed
+
+### ** Examples
+
+#Simple two path mediation
+#Write expression of indirect effect
+med <- 'a*b'
+#Paramter values from analyses
+aparam <- 1
+bparam<-2
+#Asymptotic covariance matrix from analyses
+AC <- matrix(c(.01,.00002,
+               .00002,.02), nrow=2, byrow=TRUE)
+#Compute CI, include a plot
+monteCarloMed(med, coef1=aparam, coef2=bparam, outputValues=FALSE, plot=TRUE, ACM=AC)
+
+#Use a matrix of parameter estimates as input
+aparam<-c(1,2)
+monteCarloMed(med, coef1=aparam, outputValues=FALSE, plot=TRUE, ACM=AC)
+
+
+
+#complex mediation with two paths for the indirect effect
+#Write expression of indirect effect
+med <- 'a1*b1 + a1*b2'
+#Paramter values and standard errors from analyses
+aparam <- 1
+b1param<-2
+b2param<-1
+#Asymptotic covariance matrix from analyses
+AC <- matrix(c(1,.00002, .00003,
+                    .00002,1, .00002,
+					.00003, .00002, 1), nrow=3, byrow=TRUE)
+#Compute CI do not include a plot
+monteCarloMed(med, coef1=aparam, coef2=b1param, coef3=b2param, ACM=AC)
+
+
+
+cleanEx()
 nameEx("moreFitIndices")
 ### * moreFitIndices
 
