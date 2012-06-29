f6a8a52e48bd92a7b04ef8470776ebf3c83fbe07
Sunthud Pornprasertmanit <psunthud@gmail.com>
2012-06-29 18:55:29 -0500

    0.1-5 for real

--- semTools.Rcheck/semTools-Ex.R
+++ semTools.Rcheck/semTools-Ex.R
@@ -386,6 +386,43 @@ skew(1:5)
 
 
 
+cleanEx()
+nameEx("splitSample")
+### * splitSample
+
+flush(stderr()); flush(stdout())
+
+### Name: splitSample
+### Title: Randomly Split a Data Set into Halves
+### Aliases: splitSample
+
+### ** Examples
+
+#### Input is .dat file
+#splitSample("C:/Users/Default/Desktop/MYDATA.dat")
+#### Output saved to "C:/Users/Default/Desktop/" in .dat format
+#### Names are "MYDATA_s1.dat" and "MYDATA_s2.dat"
+
+#### Input is R object
+##Split C02 dataset from the datasets package
+library(datasets)
+splitMyData <- splitSample(CO2, path="object")
+summary(splitMyData[[1]])
+summary(splitMyData[[2]])
+#### Output object splitMyData becomes list of output data sets
+
+#### Input is .dat file in "C:/" folder
+#splitSample("C:/testdata.dat", path = "C:/Users/Default/Desktop/", type = "csv")
+#### Output saved to "C:/Users/Default/Desktop/" in .csv format
+#### Names are "testdata_s1.csv" and "testdata_s2.csv"
+
+#### Input is R object
+#splitSample(myData, path = "C:/Users/Default/Desktop/", name = "splitdata")
+#### Output saved to "C:/Users/Default/Desktop/" in .dat format
+#### Names are "splitdata_s1.dat" and "splitdata_s2.dat"
+
+
+
 ### * <FOOTER>
 ###
 cat("Time elapsed: ", proc.time() - get("ptime", pos = 'CheckExEnv'),"\n")
