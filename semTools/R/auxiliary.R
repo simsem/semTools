36fd5efaa1b33586bdf7888997d7f6d069e9bead
Terry <inherimage@gmail.com>
2015-04-04 20:21:58 -0500

    Update auxiliary.R
    replaced "eq.id" column with "plabel" column in ParTable

--- semTools/R/auxiliary.R
+++ semTools/R/auxiliary.R
@@ -160,7 +160,7 @@ attachConstraint <- function(pt, con) {
 		pt$ustart <- c(pt$ustart, con$ustart)
 		pt$exo <- c(pt$exo, con$exo)
 		pt$label <- c(pt$label, con$label)
-		pt$eq.id <- c(pt$eq.id, con$eq.id)
+		pt$plabel <- c(pt$plabel, con$plabel)
 	}
 	pt
 }
@@ -191,7 +191,7 @@ attachPT <- function(pt, lhs, op, rhs, ngroups, symmetric=FALSE, exo=FALSE, fixe
 	pt$ustart <- c(pt$ustart, rep(ustart, num))
 	pt$exo <- c(pt$exo, rep(as.numeric(exo), num))
 	pt$label <- c(pt$label, rep("", num))
-	pt$eq.id <- c(pt$eq.id, rep(0L, num))
+	pt$plabel <- c(pt$plabel, rep("", num))
 	return(pt)
 }
 
@@ -210,7 +210,7 @@ nullAuxiliary <- function(aux, indName, covName=NULL, meanstructure, ngroups) {
 	pt$ustart <- rep(NA, num)
 	pt$exo <- rep(0, num)
 	pt$label <- rep("", num)
-	pt$eq.id <- rep(0, num)
+	pt$plabel <- rep("", num)
 	if(meanstructure) {
 		pt$lhs <- rep(rep(indName, ngroups), 2)
 		pt$op <- rep(c("~~", "~1"), each=num/2)
