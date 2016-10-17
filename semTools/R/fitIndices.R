6c2c65221dfaef2f0ccc6c979a15f02a4d9c6078
t129j179 <inherimage@gmail.com>
2016-10-17 17:09:16 +0200

    added chisqSmallN

--- semTools/R/fitIndices.R
+++ semTools/R/fitIndices.R
@@ -1,12 +1,17 @@
 ## Title: Compute more fit indices
-## Author: Sunthud Pornprasertmanit <psunthud@ku.edu>, Aaron Boulton <aboulton@ku.edu>, Ruben Arslan <rubenarslan@gmail.com>, Terrence Jorgensen <TJorgensen314@gmail.com>
+## Authors: Terrence Jorgensen <TJorgensen314@gmail.com>
+##          Sunthud Pornprasertmanit <psunthud@ku.edu>,
+##          Aaron Boulton <aboulton@ku.edu>,
+##          Ruben Arslan <rubenarslan@gmail.com>
+## Last updated: 17 October 2016
 ## Description: Calculations for promising alternative fit indices
-##----------------------------------------------------------------------------##
+##----------------------------------------------------------------------------
 
 moreFitIndices <- function(object, fit.measures = "all", nPrior = 1) {
   ## check for validity of user-specified "fit.measures" argument
-  fit.choices <- c("gammaHat","adjGammaHat","baseline.rmsea","aic.smallN","bic.priorN","hqc",
-                   "sic","gammaHat.scaled","adjGammaHat.scaled","baseline.rmsea.scaled")
+  fit.choices <- c("gammaHat","adjGammaHat","baseline.rmsea",
+                   "gammaHat.scaled","adjGammaHat.scaled","baseline.rmsea.scaled",
+                   "aic.smallN","bic.priorN","hqc","sic")
   flags <- setdiff(fit.measures, c("all", fit.choices))
   if (length(flags)) stop(paste("Argument 'fit.measures' includes invalid options:",
                                 paste(flags, collapse = ", "),
@@ -20,15 +25,15 @@ moreFitIndices <- function(object, fit.measures = "all", nPrior = 1) {
   p <- length(lavaan::lavNames(object, type = "ov", group = 1))
   # Get the number of parameters
   nParam <- fit["npar"]
-	
+
   # Get number of observations
   n <- lavaan::lavInspect(object, "ntotal")
   # Find the number of groups
   ngroup <- lavaan::lavInspect(object, "ngroups")
-  
+
   # Calculate -2*log(likelihood)
   f <- -2 * fit["logl"]
-  
+
   # Compute fit indices
   result <- list()
   if (length(grep("gamma", fit.measures, ignore.case = TRUE))) {
@@ -58,29 +63,29 @@ moreFitIndices <- function(object, fit.measures = "all", nPrior = 1) {
   unlist(result[fit.measures])
 }
 
-nullRMSEA <- function (object, scaled = FALSE, silent = FALSE) { 
+nullRMSEA <- function (object, scaled = FALSE, silent = FALSE) {
 	# return RMSEA of the null model, warn if it is lower than 0.158, because it makes the TLI/CLI hard to interpret
 	test <- lavaan::lavInspect(object, "options")$test
-	
+
 	fits <- lavaan::fitMeasures(object)
 	N <- lavaan::lavInspect(object, "ntotal") # sample size
-	
+
 	X2 <- as.numeric ( fits['baseline.chisq'] ) # get baseline chisq
-	df <- as.numeric ( fits['baseline.df'] ) # get baseline df 
+	df <- as.numeric ( fits['baseline.df'] ) # get baseline df
 	G <- lavaan::lavInspect(object, "ngroups") # number of groups
-	
+
 	### a simple rip from fit.measures.R in lavaan's codebase.
 	N.RMSEA <- max(N, X2*4) # Check with lavaan
         # RMSEA
 	if(df > 0) {
 		if(scaled) {
 			d <- sum(diag(lavaan::lavInspect(object, "UGamma")))
-		} 
+		}
 		if(lavaan::lavInspect(object, "options")$mimic %in% c("Mplus", "lavaan")) {
 			GG <- 0
 			RMSEA <- sqrt( max( c((X2/N)/df - 1/(N-GG), 0) ) ) * sqrt(G)
 			if(scaled && test != "scaled.shifted") {
-				RMSEA.scaled <- 
+				RMSEA.scaled <-
 					 sqrt( max( c((X2/N)/d - 1/(N-GG), 0) ) ) * sqrt(G)
 			} else if(test == "scaled.shifted") {
 				RMSEA.scaled <-
@@ -99,7 +104,7 @@ nullRMSEA <- function (object, scaled = FALSE, silent = FALSE) {
 		RMSEA <- RMSEA.scaled
 	}
 	if(!silent) {
-		if(RMSEA < 0.158 ) { 
+		if(RMSEA < 0.158 ) {
 			cat(paste0("TLI and other incremental fit indices may not be that informative, because the RMSEA of the baseline model is lower than 0.158 (Kenny, Kaniskan, & McCoach, 2011). The baseline RMSEA is ",round(RMSEA,3), "\n"))
 		} else {
 			cat(paste0("Baseline RMSEA: ",round(RMSEA,3), "\n"))
@@ -132,3 +137,49 @@ sic <- function(f, lresults = NULL) {
   ## return SIC
   f + log(DET)
 }
+
+## small-sample adjustment for (delta) chi-squared test statistic
+chisqSmallN <- function(fit0, fit1 = NULL, ...) {
+  ## if there are 2 models, order them by DF
+  if (!is.null(fit1)) {
+    DF0 <- lavaan::fitMeasures(fit0, "df")
+    DF1 <- lavaan::fitMeasures(fit1, "df")
+    if (DF0 == DF1) stop("Models have the same degrees of freedom.")
+    parent <- which.min(c(DF0, DF1))
+    if (parent == 1L) {
+      parent <- fit0
+      fit0 <- fit1
+      fit1 <- parent
+    }
+    #if (min(c(DF0, DF1)) == 0L) fit1 <- NULL
+  }
+  ## calculate k-factor correction
+  N <- lavaan::lavInspect(fit0, "ntotal")
+  if (!lavaan::lavInspect(fit0, "options")$sample.cov.rescale) N <- N - 1
+  P <- length(lavaan::lavNames(fit0))
+  K <- length(lavaan::lavNames(fit0, type = "lv")) # count latent factors
+  if (!is.null(fit1)) {
+    N1 <- lavaan::lavInspect(fit1, "ntotal")
+    if (!lavaan::lavInspect(fit1, "options")$sample.cov.rescale) N1 <- N1 - 1
+    if (N != N1) stop("Unequal sample sizes")
+    if (P != length(lavaan::lavNames(fit1))) stop("Unequal number of variables")
+    K <- max(K, length(lavaan::lavNames(fit1, type = "lv")))
+  }
+  kc <- 1 - ((2*P + 4*K + 5) / (6*N))
+  if (is.null(fit1)) {
+    scaled <- lavaan::lavInspect(fit0, "options")$test %in%
+      c("satorra.bentler","yuan.bentler","mean.var.adjusted","scaled.shifted")
+    chi <- lavaan::fitMeasures(fit0, if (scaled) "chisq.scaled" else "chisq")
+    DF <- lavaan::fitMeasures(fit0, "df")
+  } else {
+    AOV <- lavaan::lavTestLRT(fit0, fit1, ...)
+    chi <- AOV[["Chisq diff"]][2]
+    DF <- AOV[["Df diff"]][2]
+  }
+  out <- c(naive.chisq = chi, `k-factor` = kc, adj.chisq = chi*kc,
+           df = DF, pvalue = pchisq(chi*kc, DF, lower.tail = FALSE))
+  class(out) <- c("lavaan.vector","numeric")
+  out
+}
+
+
