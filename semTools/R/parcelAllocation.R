e0252b27d6d8212b2432c79f82f75fc17d0d30fd
t129j179 <inherimage@gmail.com>
2017-06-07 18:45:45 +0200

    clarify parcelAllocation argument descriptions

--- semTools/R/parcelAllocation.R
+++ semTools/R/parcelAllocation.R
@@ -1,13 +1,13 @@
 ### Corbin Quick & Alex Schoemann
-### Last updated: 4 April 2017
+### Last updated: 7 June 2017
 
 
 #' Random Allocation of Items to Parcels in a Structural Equation Model
-#' 
+#'
 #' This function generates a given number of randomly generated item-to-parcel
 #' allocations, fits a model to each allocation, and provides averaged results
 #' over all allocations.
-#' 
+#'
 #' This function implements the random item to parcel allocation procedure
 #' described in Sterba (2011) and Sterba and MacCallum (2010). The function
 #' takes a single data set with item level data, randomly assigns items to
@@ -16,9 +16,9 @@
 #' random allocations. Results from all fitted models are summarized and
 #' output. For further details on the benefits of the random allocation of
 #' itesm to parcels see Sterba (2011) and Sterba and MccCallum (2010).
-#' 
+#'
 #' @importFrom stats sd runif
-#' 
+#'
 #' @param nPerPar A list in which each element is a vector corresponding to
 #' each factor indicating sizes of parcels. If variables are left out of
 #' parceling, they should not be accounted for here (there should NOT be
@@ -37,9 +37,10 @@
 #' If the data has missing values multiple imputation before parceling is
 #' recommended.
 #' @param names (Optional) A character vector containing the names of parceled
-#' variables.
+#' variables. These names should appear as indicators in \code{syntax}.
 #' @param leaveout A vector of variables to be left out of randomized
-#' parceling. Either variable names or column numbers are allowed.
+#' parceling. Either variable names or column numbers are allowed. These names
+#' should appear as columns in \code{dataset} and be listed in \code{facPlc}.
 #' @param \dots Additional arguments to be passed to \link{lavaan}
 #' @return \item{Estimates}{A data frame containing results related to
 #' parameter estimates with columns corresponding to parameter names, average
@@ -60,27 +61,27 @@
 #' and the range of each fit index across allocations.}
 #' @author
 #'  Corbin Quick (University of Michigan; \email{corbinq@@umich.edu})
-#'  
+#'
 #' Alexander M. Schoemann (East Carolina University; \email{schoemanna@@ecu.edu})
-#' 
+#'
 #' @seealso \code{\link{PAVranking}}, \code{\link{poolMAlloc}}
 #' @references Sterba, S. K. (2011). Implications of parcel-allocation
 #' variability for comparing fit of item-solutions and parcel-solutions.
 #' \emph{Structural Equation Modeling, 18}(4), 554-577.
 #' doi:10.1080/10705511.2011.607073
-#' 
+#'
 #' Sterba, S. K.m & MacCallum, R. C. (2010). Variability in parameter estimates
 #' and model fit across random allocations of items to parcels.
 #' \emph{Multivariate Behavioral Research, 45}(2), 322-358.
 #' doi:10.1080/00273171003680302
 #' @examples
-#' 
+#'
 #' ## Fit 3 factor CFA to simulated data.
 #' ## Each factor has 9 indicators that are randomly parceled into 3 parcels
 #' ## lavaan syntax for the model to be fit to parceled data
 #' library(lavaan)
-#' 
-#' syntax <- 'La =~ V1 + V2 + V3 
+#'
+#' syntax <- 'La =~ V1 + V2 + V3
 #'            Lb =~ V4 + V5 + V6
 #' '
 #' ## Parcel and fit data 20 times.
@@ -89,21 +90,21 @@
 #' name2 <- colnames(simParcel)[10:18]
 #' parcelAllocation(list(c(3,3,3),c(3,3,3)), list(name1, name2), nAlloc = 20,
 #'                  syntax = syntax, dataset = simParcel)
-#' 
+#'
 #' @export
 parcelAllocation <- function(nPerPar, facPlc, nAlloc = 100, syntax, dataset,
                              names = "default", leaveout = 0, ...) {
   if (is.character(dataset)) dataset <- utils::read.csv(dataset)
   dataset <- as.matrix(dataset)
-  
+
   if (nAlloc < 2) stop("Minimum of two allocations required.")
-  
+
   if (is.list(facPlc)) {
-    
+
     if (is.numeric(facPlc[[1]][1]) == FALSE) {
       facPlcb <- facPlc
       Namesv <- colnames(dataset)
-      
+
       for (i in 1:length(facPlc)) {
         for (j in 1:length(facPlc[[i]])) {
           facPlcb[[i]][j] <- match(facPlc[[i]][j], Namesv)
@@ -112,10 +113,10 @@ parcelAllocation <- function(nPerPar, facPlc, nAlloc = 100, syntax, dataset,
       }
       facPlc <- facPlcb
     }
-    
+
     # facPlc2 <- rep(0, sum(sapply(facPlc, length)))
     facPlc2 <- rep(0, ncol(dataset))
-    
+
     for (i in 1:length(facPlc)) {
       for (j in 1:length(facPlc[[i]])) {
         facPlc2[facPlc[[i]][j]] <- i
@@ -123,27 +124,27 @@ parcelAllocation <- function(nPerPar, facPlc, nAlloc = 100, syntax, dataset,
     }
     facPlc <- facPlc2
   }
-  
+
   if (leaveout != 0) {
-    
+
     if (is.numeric(leaveout) == FALSE) {
       leaveoutb <- rep(0, length(leaveout))
       Namesv <- colnames(dataset)
-      
+
       for (i in 1:length(leaveout)) {
         leaveoutb[i] <- match(leaveout[i], Namesv)
       }
       leaveout <- as.numeric(leaveoutb)
-      
+
     }
-    
+
     k1 <- 0.001
     for (i in 1:length(leaveout)) {
       facPlc[leaveout[i]] <- facPlc[leaveout[i]] + k1
       k1 <- k1 + 0.001
     }
   }
-  
+
   if (0 %in% facPlc == TRUE) {
     Zfreq <- sum(facPlc == 0)
     for (i in 1:Zfreq) {
@@ -154,7 +155,7 @@ parcelAllocation <- function(nPerPar, facPlc, nAlloc = 100, syntax, dataset,
     ## this allows for unused variables in dataset, which are specified by
     ## zeros, and deleted
   }
-  
+
   if (is.list(nPerPar)) {
     nPerPar2 <- c()
     for (i in 1:length(nPerPar)) {
@@ -163,16 +164,16 @@ parcelAllocation <- function(nPerPar, facPlc, nAlloc = 100, syntax, dataset,
     }
     nPerPar <- nPerPar2
   }
-  
+
   Npp <- c()
   for (i in 1:length(nPerPar)) Npp <- c(Npp, rep(i, nPerPar[i]))
-  
+
   Locate <- sort(round(facPlc))
   Maxv <- max(Locate) - 1
-  
+
   if (length(Locate) != length(Npp)) stop("Parcels incorrectly specified.",
                                           " Check input!")
-  
+
   if (Maxv > 0) {
     ## Bug was here. With 1 factor Maxv=0. Skip this with a single factor
     for (i in 1:Maxv) {
@@ -185,27 +186,27 @@ parcelAllocation <- function(nPerPar, facPlc, nAlloc = 100, syntax, dataset,
   ## which each variable belongs vector, factor to which each variables belongs
   ## if variables are in the same parcel, but different factors error message
   ## given in output
-  
+
   Onevec <- facPlc - round(facPlc)
   NleaveA <- length(Onevec) - sum(Onevec == 0)
   NleaveP <- sum(nPerPar == 1)
-  
+
   if (NleaveA < NleaveP) warning("Single-variable parcels have",
                                  " been requested. Check input!")
-  
+
   if (NleaveA > NleaveP) warning("More non-parceled variables",
                                  " have been requested than provided for in",
                                  " parcel vector. Check input!")
-  
+
   if (length(names) > 1) {
     if (length(names) != length(nPerPar))
       warning("Number of parcel names provided not equal to",
               " number of parcels requested. Check input!")
   }
-  
+
   if (NA %in% dataset == TRUE) warning("Missing data detected. Prior",
                                        " multiple imputation recommended.")
-  
+
   Data <- c(1:ncol(dataset))
   ## creates a vector of the number of indicators e.g. for three indicators, c(1, 2,
   ## 3)
@@ -217,62 +218,62 @@ parcelAllocation <- function(nPerPar, facPlc, nAlloc = 100, syntax, dataset,
   ## scalar, number of parcels
   Rmize <- runif(Nindicators, 1, Nindicators)
   ## create vector of randomly ordered numbers, length of number of indicators
-  
+
   Data <- rbind(facPlc, Rmize, Data)
   ## 'Data' becomes object of three rows, consisting of 1) factor to which each
   ## indicator belongs (in order to preserve indicator/factor assignment during
   ## randomization) 2) randomly order numbers 3) indicator number
-  
+
   Results <- matrix(numeric(0), nAlloc, Nindicators)
   ## create empty matrix for parcel allocation matrix
-  
+
   Pin <- nPerPar[1]
   for (i in 2:length(nPerPar)) {
-    
+
     Pin <- c(Pin, nPerPar[i] + Pin[i - 1])
     ## creates vector which indicates the range of columns (endpoints) in each parcel
   }
-  
+
   for (i in 1:nAlloc) {
     Data[2, ] <- runif(Nindicators, 1, Nindicators)
     ## Replace second row with newly randomly ordered numbers
-    
+
     Data <- Data[, order(Data[2, ])]
     ## Order the columns according to the values of the second row
-    
+
     Data <- Data[, order(Data[1, ])]
     ## Order the columns according to the values of the first row in order to preserve
     ## factor assignment
-    
+
     Results[i, ] <- Data[3, ]
     ## assign result to allocation matrix
   }
-  
+
   Alpha <- rbind(Results[1, ], dataset)
   ## bind first random allocation to dataset 'Alpha'
-  
+
   Allocations <- list()
   ## create empty list for allocation data matrices
-  
+
   for (i in 1:nAlloc) {
-    
+
     Ineff <- rep(NA, ncol(Results))
     Ineff2 <- c(1:ncol(Results))
     for (inefficient in 1:ncol(Results)) {
       Ineff[Results[i, inefficient]] <- Ineff2[inefficient]
     }
-    
+
     Alpha[1, ] <- Ineff
     ## replace first row of dataset matrix with row 'i' from allocation matrix
-    
+
     Beta <- Alpha[, order(Alpha[1, ])]
     ## arrangle dataset columns by values of first row assign to temporary matrix
     ## 'Beta'
-    
+
     Temp <- matrix(NA, nrow(dataset), Npar)
     ## create empty matrix for averaged parcel variables
-    
-    TempAA <- if (length(1:Pin[1]) > 1) 
+
+    TempAA <- if (length(1:Pin[1]) > 1)
       Beta[2:nrow(Beta), 1:Pin[1]] else cbind(Beta[2:nrow(Beta), 1:Pin[1]],
                                               Beta[2:nrow(Beta), 1:Pin[1]])
     Temp[, 1] <- rowMeans(TempAA)
@@ -280,21 +281,21 @@ parcelAllocation <- function(nPerPar, facPlc, nAlloc = 100, syntax, dataset,
     for (al in 2:Npar) {
       Plc <- Pin[al - 1] + 1
       ## placeholder variable for determining parcel width
-      TempBB <- if (length(Plc:Pin[al]) > 1) 
+      TempBB <- if (length(Plc:Pin[al]) > 1)
         Beta[2:nrow(Beta), Plc:Pin[al]] else cbind(Beta[2:nrow(Beta), Plc:Pin[al]],
                                                    Beta[2:nrow(Beta), Plc:Pin[al]])
       Temp[, al] <- rowMeans(TempBB)
       ## fill remaining columns with averages from assigned indicators
     }
-    
+
     if (length(names) > 1) {
       colnames(Temp) <- names
     }
-    
+
     Allocations[[i]] <- Temp
     ## assign result to list of parcel datasets
   }
-  
+
   if (as.vector(regexpr("/", syntax)) != -1) {
     replist <- matrix(NA, nAlloc, 1)
     for (i in 1:nAlloc) {
@@ -311,12 +312,12 @@ parcelAllocation <- function(nPerPar, facPlc, nAlloc = 100, syntax, dataset,
     utils::write.table(replist, paste(syntax, "parcelrunsreplist.dat", sep = ""),
                        quote = FALSE, row.names = FALSE, col.names = FALSE)
   } else {
-    
+
     Param <- list()
     ## list for parameter estimated for each imputation
     Fitind <- list()
     ## list for fit indices estimated for each imputation
-    
+
     for (i in 1:nAlloc) {
       data <- as.data.frame(Allocations[[i]], row.names = NULL, optional = FALSE)
       ## convert allocation matrix to dataframe for model estimation
@@ -324,29 +325,29 @@ parcelAllocation <- function(nPerPar, facPlc, nAlloc = 100, syntax, dataset,
       ## estimate model in lavaan
       Param[[i]] <- lavaan::parameterEstimates(fit)
       ## assign allocation parameter estimates to list
-      Fitind[[i]] <- lavaan::fitMeasures(fit, c("chisq", "df", "cfi", "tli", 
+      Fitind[[i]] <- lavaan::fitMeasures(fit, c("chisq", "df", "cfi", "tli",
                                                 "rmsea", "srmr"))
       ## assign allocation parameter estimates to list
     }
-    
+
     Parmn <- Param[[1]]
     ## assign first parameter estimates to mean dataframe
-    
+
     ParSE <- matrix(NA, nrow(Parmn), nAlloc)
     ParSEmn <- Parmn[, 5]
-    
+
     Parsd <- matrix(NA, nrow(Parmn), nAlloc)
     ## assign parameter estimates for S.D. calculation
-    
+
     Fitmn <- Fitind[[1]]
     ## assign first fit indices to mean dataframe
-    
+
     Fitsd <- matrix(NA, length(Fitmn), nAlloc)
     ## assign fit indices for S.D. calculation
-    
+
     Sigp <- matrix(NA, nrow(Parmn), nAlloc)
     ## assign p-values to calculate percentage significant
-    
+
     for (i in 1:nAlloc) {
       Parsd[, i] <- Param[[i]][, 4]
       ## assign parameter estimates for S.D. estimation
@@ -365,31 +366,31 @@ parcelAllocation <- function(nPerPar, facPlc, nAlloc = 100, syntax, dataset,
       if (i > 1) Fitmn <- Fitmn + Fitind[[i]]
       ## add together all fit indices
     }
-    
-    
+
+
     Sigp <- Sigp + 0.45
     Sigp <- apply(Sigp, c(1, 2), round)
     Sigp <- 1 - as.vector(rowMeans(Sigp))
     ## calculate percentage significant parameters
-    
+
     Parsum <- cbind(apply(Parsd, 1, sd), apply(Parsd, 1, max),
                     apply(Parsd, 1, min),
                     apply(Parsd, 1, max) - apply(Parsd, 1, min), Sigp)
     colnames(Parsum) <- c("S.D.", "MAX", "MIN", "Range", "% Sig")
     ## calculate parameter SD, min, max, range, bind to percentage significant
-    
+
     ParSEmn <- cbind(Parmn[, 1:3], ParSEmn / nAlloc)
     ParSEfn <- cbind(ParSEmn, apply(ParSE, 1, sd), apply(ParSE, 1, max),
                      apply(ParSE, 1, min),
                      apply(ParSE, 1, max) - apply(ParSE, 1, min))
     colnames(ParSEfn) <- c("lhs","op","rhs","Avg SE","S.D.","MAX","MIN","Range")
-    
+
     Fitsum <- cbind(apply(Fitsd, 1, sd), apply(Fitsd, 1, max),
                     apply(Fitsd, 1, min),
                     apply(Fitsd, 1, max) - apply(Fitsd, 1, min))
     rownames(Fitsum) <- c("chisq", "df", "cfi", "tli", "rmsea", "srmr")
     ## calculate fit S.D., minimum, maximum, range
-    
+
     Parmn[, 4:ncol(Parmn)] <- Parmn[, 4:ncol(Parmn)] / nAlloc
     ## divide totalled parameter estimates by number allocations
     Parmn <- Parmn[, 1:4]
@@ -398,19 +399,19 @@ parcelAllocation <- function(nPerPar, facPlc, nAlloc = 100, syntax, dataset,
     ## bind parameter average estimates to cross-allocation information
     Fitmn <- Fitmn/nAlloc
     ## divide totalled fit indices by number allocations
-    
+
     Fitsum <- cbind(Fitmn, Fitsum)
     colnames(Fitsum) <- c("Avg Ind", "S.D.", "MAX", "MIN", "Range")
     ## bind to fit averages
-    
+
     ParSEfn[, 4:8] <- apply(ParSEfn[, 4:8], 2, round, digits = 3)
     Parmn[, 4:9] <- apply(Parmn[, 4:9], 2, round, digits = 3)
     Fitsum <- apply(Fitsum, 2, round, digits = 3)
     ## round output to three digits
-    
+
     Output <- list(Parmn, ParSEfn, Fitsum)
     names(Output) <- c("Estimates", "SE", "Fit")
-    
+
     return(Output)
   }
 }
