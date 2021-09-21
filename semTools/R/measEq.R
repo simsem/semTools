77cf169f3be02cc1a7fd8e245d8f570ecc3a23f4
TDJorgensen <TJorgensen314@gmail.com>
2021-09-21 15:03:38 +0200

    similar fix as #85 for writing Mplus thresholds

--- semTools/R/measEq.R
+++ semTools/R/measEq.R
@@ -1,5 +1,5 @@
 ### Terrence D. Jorgensen
-### Last updated: 7 July 2021
+### Last updated: 21 September 2021
 ### lavaan model syntax-writing engine for new measEq() to replace
 ### measurementInvariance(), measurementInvarianceCat(), and longInvariance()
 
@@ -3124,7 +3124,7 @@ write.mplus.syntax <- function(object, group = 1, params = NULL) {
     allThrNames <- unique(do.call(c, lapply(object@labels, "[[", i = "tau")))
     ## loop over ordinal indicators, specify set on a single line
     for (i in object@ordered) {
-      iThr <- grep(i, rownames(object@labels[[group]]$tau))
+      iThr <- grep(paste0(i, "\\|"), rownames(object@labels[[group]]$tau))
       specify <- object@specify[[group]]$tau[iThr, 1] #NOTE: These are now vectors
       values  <- object@values[[ group]]$tau[iThr, 1]
       labels  <- object@labels[[ group]]$tau[iThr, 1]
@@ -3182,7 +3182,7 @@ write.mplus.syntax <- function(object, group = 1, params = NULL) {
       E.lab  <- ""
 
       i.var <- c(i.var, paste0("{", RR, ifelse(is.na(E.val), yes = "*",
-                                              no = paste0("@", E.val)), "};"))
+                                               no = paste0("@", E.val)), "};"))
     } else {
       ## RESIDUAL VARIANCES
       E.val  <- object@values[[group]]$theta[RR, RR]
