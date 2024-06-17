3046d1806f2484c21ef3fe87343ece1a98ba0b3b
TDJorgensen <TJorgensen314@gmail.com>
2024-06-17 11:44:25 +0200

    warn when semTools masks lavaan.mi, instruct fix

--- semTools/R/zzz.R
+++ semTools/R/zzz.R
@@ -5,11 +5,27 @@
     packageStartupMessage("This is ", paste(pkgname, version))
     packageStartupMessage("All users of R (or SEM) are invited to submit functions or ideas for functions.")
     packageStartupMessage("###############################################################################")
+
+    ## if lavaan.mi is already loaded, warn users to use :: or switch order
+    if ("lavaan.mi" %in% names(utils::sessionInfo()$otherPkgs)) {
+      packageStartupMessage(" ")
+      packageStartupMessage("The lavaan.mi package was already attached when semTools was attached.")
+      packageStartupMessage("To access lavaan.mi functions masked by semTools, use the double-colon:")
+      packageStartupMessage(" ")
+      packageStartupMessage("\t lavaan.mi::cfa.mi() or lavaan.mi::lavTestLRT.mi()\n")
+      packageStartupMessage("It is preferable to first attach semTools, then attach lavaan.mi, but")
+      packageStartupMessage("you can change the path order by detaching and reloading lavaan.mi,")
+      packageStartupMessage("using this syntax:\n")
+      packageStartupMessage('\t detach("package:lavaan.mi", unload = TRUE)')
+      packageStartupMessage('\t library(lavaan.mi) \n')
+      packageStartupMessage("Then the deprecated semTools::lavaan.mi functionality will be masked.")
+    }
 }
 
 .onLoad <- function(libname, pkgname) {
-    if (requireNamespace("emmeans", quietly = TRUE)){
-        emmeans::.emm_register("lavaan", pkgname)
-    }
+  ## "register" emmeans functionality
+  if (requireNamespace("emmeans", quietly = TRUE)){
+      emmeans::.emm_register("lavaan", pkgname)
+  }
 }
 
